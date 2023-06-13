c213d7856a6a1c295f73fab152394d2c7b0774cc
blackneb <antenehcs@gmail.com>
2023-06-13 11:36:53 +0300

    shopping cart

---
+++ src/ShoppingCart.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Anteneh/OWASP ZAP/webdriver/windows/32/chromedriver.exe");
+
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        //Maximize window
+        driver.manage().window().maximize();
+
+        //Launch the Site.
+        driver.get("https://demowebshop.tricentis.com/login");
+
+        //Enter values into form and submit
+        driver.findElement(By.id("Email")).sendKeys("ant12@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("123456");
+        driver.findElement(By.id("RememberMe")).click();
+
+        driver.findElement(By.xpath("//input[@value='Log in']")).click();
+
+        List<WebElement> list2 =driver.findElements(By.xpath("//input[@value='Add to cart']"));
+
+
+        list2.get(1).click();
+
+        String result = driver.findElement(By.className("content")).getText();
+
+        if( result.equals("The product has been added to your shopping cart") ) {
+            System.out.println("Test Success");
+        } else {
+            System.out.println("Test Failed");
+        }
+
+        //Close the browser
+        //driver.quit();
+    }
+}
